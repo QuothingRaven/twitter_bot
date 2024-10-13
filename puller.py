71f620069610501fca97ea9ebee15ff9b8f584f1
QuothingRaven <95989700+QuothingRaven@users.noreply.github.com>
2024-10-13 16:51:56 -0700

    Update puller.py

--- puller.py
+++ puller.py
@@ -10,7 +10,23 @@ load_dotenv()
 intents = discord.Intents.default()
 intents.members = True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+class MyBot(commands.Bot):
+    def __init__(self):
+        super().__init__(command_prefix='!', intents=intents)
+
+    async def fetch_new_posts(self):
+        while True:
+            await self.schedule_post_check(30 * 60)  # Check for new posts every 30 minutes
+
+    async def schedule_post_check(self, delay):
+        await asyncio.sleep(delay)
+        await self.fetch_new_posts()
+
+    async def on_ready(self):
+        print(f'{self.user.name} has connected to Discord!')
+        await self.fetch_new_posts()
+
+bot = MyBot()
 
 x_api_key = os.getenv("X_API_KEY")
 x_api_secret = os.getenv("X_API_SECRET")
@@ -22,10 +38,6 @@ headers = {
     'Authorization': f'Bearer {x_api_key}',
     'Content-Type': 'application/json',
 }
-
-@bot.event
-async def on_ready():
-    print(f'{bot.user.name} has connected to Discord!')
 
 @bot.event
 async def on_message(message):
@@ -40,18 +52,5 @@ async def on_message(message):
         post = response.json()['data'][0]
         post_text = post['text']
         await message.channel.send(f'{post_text}')
-
-async def fetch_new_posts(self):
-    while True:
-        await self.schedule_post_check(30 * 60)  # Check for new posts every 30 minutes
-
-async def schedule_post_check(self, delay):
-    await self.loop.call_later(delay, self.fetch_new_posts)
-
-@bot.event
-async def on_ready():
-    await self.fetch_new_posts()
-
-from discord import Channel
 
 bot.run(discord_token)
