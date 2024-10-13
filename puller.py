0902c55d90a0c2f7f8ebc586b2482536e3705c92
QuothingRaven <95989700+QuothingRaven@users.noreply.github.com>
2024-10-13 16:25:13 -0700

    Update puller.py

--- puller.py
+++ puller.py
@@ -11,16 +11,16 @@ load_dotenv()
 # Discord bot token
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 
-# Twitter API credentials
-TWITTER_API_KEY = os.getenv('TWITTER_API_KEY')
-TWITTER_API_SECRET_KEY = os.getenv('TWITTER_API_SECRET_KEY')
-TWITTER_ACCESS_TOKEN = os.getenv('TWITTER_ACCESS_TOKEN')
-TWITTER_ACCESS_TOKEN_SECRET = os.getenv('TWITTER_ACCESS_TOKEN_SECRET')
+# X.com API credentials
+X_API_KEY = os.getenv('X_API_KEY')
+X_API_SECRET = os.getenv('X_API_SECRET')
+X_ACCESS_TOKEN = os.getenv('X_ACCESS_TOKEN')
+X_ACCESS_TOKEN_SECRET = os.getenv('X_ACCESS_TOKEN_SECRET')
 
-# Twitter account to monitor
-TWITTER_ACCOUNT = os.getenv('TWITTER_ACCOUNT')
+# X.com account to monitor
+X_ACCOUNT = os.getenv('X_ACCOUNT')
 
-# Discord channel ID to post tweets
+# Discord channel ID to post X.com posts
 DISCORD_CHANNEL_ID = int(os.getenv('DISCORD_CHANNEL_ID'))
 
 # Set up Discord bot with intents
@@ -28,9 +28,9 @@ intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-# Set up Twitter API client
-auth = tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_API_SECRET_KEY)
-auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
+# Set up X.com API client
+auth = tweepy.OAuthHandler(X_API_KEY, X_API_SECRET)
+auth.set_access_token(X_ACCESS_TOKEN, X_ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
 @bot.event
@@ -43,29 +43,29 @@ async def check_posts():
     channel = bot.get_channel(DISCORD_CHANNEL_ID)
     if channel:
         try:
-            tweets = api.user_timeline(screen_name=TWITTER_ACCOUNT, count=5, tweet_mode='extended')
+            posts = api.user_timeline(screen_name=X_ACCOUNT, count=5, tweet_mode='extended')
 
-            for tweet in reversed(tweets):
-                # Check if the tweet is new
-                if tweet.created_at.replace(tzinfo=timezone.utc) > bot.last_checked:
-                    embed = discord.Embed(description=tweet.full_text, color=0x1DA1F2)
-                    embed.set_author(name=f'{tweet.user.name} (@{tweet.user.screen_name})', icon_url=tweet.user.profile_image_url)
-                    embed.set_footer(text=f'Twitter • {tweet.created_at.strftime("%Y-%m-%d %H:%M:%S")}')
+            for post in reversed(posts):
+                # Check if the post is new
+                if post.created_at.replace(tzinfo=timezone.utc) > bot.last_checked:
+                    embed = discord.Embed(description=post.full_text, color=0x1DA1F2)
+                    embed.set_author(name=f'{post.user.name} (@{post.user.screen_name})', icon_url=post.user.profile_image_url)
+                    embed.set_footer(text=f'X.com • {post.created_at.strftime("%Y-%m-%d %H:%M:%S")}')
 
-                    if 'media' in tweet.entities:
-                        embed.set_image(url=tweet.entities['media'][0]['media_url_https'])
+                    if 'media' in post.entities:
+                        embed.set_image(url=post.entities['media'][0]['media_url_https'])
 
                     await channel.send(embed=embed)
 
-            if tweets:
-                bot.last_checked = tweets[0].created_at.replace(tzinfo=timezone.utc)
+            if posts:
+                bot.last_checked = posts[0].created_at.replace(tzinfo=timezone.utc)
 
         except tweepy.TweepError as e:
             print(f'Tweepy Error: {e}')
             print(f'Error code: {e.api_code if hasattr(e, "api_code") else "N/A"}')
             print(f'Error message: {e.response.text if hasattr(e, "response") else str(e)}')
         except Exception as e:
-            print(f'Error fetching tweets: {e}')
+            print(f'Error fetching posts: {e}')
             print(f'Error type: {type(e)}')
             print(f'Error details: {str(e)}')
 
